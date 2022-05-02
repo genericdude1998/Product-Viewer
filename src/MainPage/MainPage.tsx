ee9d1daba1157225806e1eb9a3185beaa9cb4b0d
vsenzo <vsenzo@hello.com>
2022-05-02 17:03:05 +0100

    add grid

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -1,9 +1,7 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Item from './Item';
 import Product from './Product';
-
 
 export default function MainPage() {
   return (
@@ -11,16 +9,16 @@ export default function MainPage() {
       <Typography variant="h1" color="initial">Title!</Typography>
       <Box sx={{ height: '100%' }}>
         <Grid container spacing={2}>
-          <Grid item xs={6}>
+          <Grid item md={3} xs={12}>
             <Product />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item md={3} xs={12}>
           <Product />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item md={3} xs={12}>
           <Product />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item md={3} xs={12}>
             <Product />
           </Grid>
         </Grid>
--- src/MainPage/Product.tsx
+++ src/MainPage/Product.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import Item from './Item'
-import { Canvas } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber';
+import { styled } from '@mui/material';
+
+const Wrapper = styled('div')`
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+`
+
+const CanvasStyles = styled('div')`
+  background-color: red;
+`
 
 export default function Product() {
   return (
-    <div>
+    <Wrapper>
+      <CanvasStyles>
         <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Item />
         </Canvas>
-      <p>Hello</p>
-    </div>
+      </CanvasStyles>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus iure, ea non accusamus vel totam vitae facilis aperiam itaque odit dolorem nesciunt eius eos alias, illum a commodi omnis cupiditate.</p>
+    </Wrapper>
   )
 }
